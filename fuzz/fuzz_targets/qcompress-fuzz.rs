1d1cf5308e0ac362f6b1a25c24baf47150a05f25
Eric Bell <48191180+ebell495@users.noreply.github.com>
2022-10-30 15:54:01 -0600

    update

--- fuzz/fuzz_targets/qcompress-fuzz.rs
+++ fuzz/fuzz_targets/qcompress-fuzz.rs
@@ -7,5 +7,5 @@ use libfuzzer_sys::fuzz_target;
 use q_compress::{auto_compress,auto_decompress,DEFAULT_COMPRESSION_LEVEL};
 
 fuzz_target!(|data: &[u8]| {
-      auto_compress(&data.iter().map(|x| x as u16).collect(), DEFAULT_COMPRESSION_LEVEL);
+      auto_compress(data.to_vec().iter().map(|x| x as &u16).collect(), DEFAULT_COMPRESSION_LEVEL);
 });
