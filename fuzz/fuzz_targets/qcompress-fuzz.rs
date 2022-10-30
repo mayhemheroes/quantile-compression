c481fd2c4ab600584daa8db630a56aa00714c4f0
Eric Bell <48191180+ebell495@users.noreply.github.com>
2022-10-30 14:25:56 -0600

    update fuzz harness

--- fuzz/fuzz_targets/qcompress-fuzz.rs
+++ fuzz/fuzz_targets/qcompress-fuzz.rs
@@ -6,10 +6,10 @@ extern crate q_compress;
 use libfuzzer_sys::fuzz_target;
 use q_compress::{auto_compress,auto_decompress,DEFAULT_COMPRESSION_LEVEL};
 
-fn convert_to_u16(in: &[u8]) -> &[u16] {
-      in.iter().map(|x| x as u16).collect()
+fn convert_to_u16(data: &[u8]) -> [u16] {
+      data.iter().map(|x| x as u16).collect()
 }
 
 fuzz_target!(|data: &[u8]| {
-      auto_compress(convert_to_u16(in), DEFAULT_COMPRESSION_LEVEL);
+      auto_compress(&convert_to_u16(data), DEFAULT_COMPRESSION_LEVEL);
 });
