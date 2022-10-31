9f5e5c27073252ef1ef784ab382a321dbae15e9f
Eric Bell <48191180+ebell495@users.noreply.github.com>
2022-10-31 02:26:57 +0000

    fix

--- fuzz/fuzz_targets/qcompress-fuzz.rs
+++ fuzz/fuzz_targets/qcompress-fuzz.rs
@@ -4,8 +4,12 @@ extern crate libfuzzer_sys;
 extern crate q_compress;
 
 use libfuzzer_sys::fuzz_target;
-use q_compress::{auto_compress,auto_decompress,DEFAULT_COMPRESSION_LEVEL};
+use q_compress::Compressor;
+j
 
 fuzz_target!(|data: &[u8]| {
-      auto_compress(data.to_vec().iter().map(|x| *x as u16).collect(), DEFAULT_COMPRESSION_LEVEL);
+      let ivec = Vec::from(data);
+      let uvec: Vec<u16> = ivec.iter().map(|x| *x as u16).collect();
+      let mut compressor = Compressor::<u16>::default();
+      let bytes = compressor.simple_compress(&uvec);
 });
